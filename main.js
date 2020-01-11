fe237a7bf80a2ab4da2ebf855a8e869f1b2039c4
Kacper Roszczak <k.roszczak@interia.pl>
2020-01-11 17:27:46 +0100

    backup

--- main.js
+++ main.js
@@ -1,122 +1,86 @@
-let space = document.querySelectorAll('.tile');
-let counter;
-const una = [];
+let tiles = document.querySelectorAll('.tile');
 const Free = [true, true ,true, true, true, true, true, true, true, true, true, true, true, true, true, true];
-const cells = ['' ,'' ,'', '', '', '', '', '', '', '', '', '', '', '', '', ''];
+const cells = []; 
+for(let i = 0; i < 16; i++) cells.push('');
+
 class tile {
     constructor(cell, number, value) {
         this.cell = cell;
         this.number = number; 
-        this.value = value;
-    }
-};
+        this.val = value;}};
+
 
 
 function GetFreeIndex(){ return Free.findIndex((v) => { return v != false; })}
 function GetRandomVal(){ return Math.random() > 0.24 ? 2 : 4;}
 function GetRandomCell(){
-
-    let freeElements = [];
-    counter = 0;
+    let freeCells = [];
+    let counter = 0;
     let random = Math.random();
 
     cells.forEach((item, index) =>{
         if(item === ''){
-            freeElements.push(index);
-            counter++;
-        }})
-        console.log(freeElements);
-        
-        if(counter == 0) return -1;
+            freeCells.push(index);
+            counter++;}})
 
     let przedzial = 1/counter;
-    for(let i = 1; i <= counter; i++)
-    {
-        if(random <= przedzial * i) {cells[freeElements[i-1]] = freeElements[i-1]; return freeElements[i-1];}
-    }
-}
+    for(let i = 1; i <= counter; i++){
+        if(random <= przedzial * i) {; return freeCells[i-1];}}}
+
 
  function CreateNewTile(){
-    let index = GetFreeIndex(Free);
-    una.push(index);
-    console.log(una);
+    let cell = GetRandomCell()
+    let index = GetFreeIndex();
     Free[index] = false;
-    return eval(`tile${index} = new tile(GetRandomCell(cells), index , GetRandomVal());`);
+    eval(`tile${index} = new tile(cell, index , GetRandomVal());`);
+    eval(`cells[tile${index}.cell] = tile${index};`);}
  
- }
- 
+
+ document.addEventListener('keydown', (k) =>{
+
+    if(k.keyCode == 40){
+        for(i = 15; i >= 12; i--){
+            if( (cells[i].val === cells[i -4].val) && (cells[i] != '') ){
+                console.log('kul');
+                cells[i].val *= 2;
+                cells[i -4] = '';
+                Free[cells[i -4].number] = true;}
+            if( (cells[i - 4].val === cells[i -8].val) && (cells[i - 4] != '') ){
+                console.log('kul2');
+                cells[i-4].val *= 2;
+                cells[i -8].val = null;
+                Free[cells[i -8].number] = true;}
+            if( (cells[i -8].val === cells[i -12].val) && (cells[i-8] != '') ){
+                console.log('kul3');
+                cells[i-8].val *= 2;
+                cells[i -12].val = null
+                Free[cells[i -12].number] = true;;
+            }}}
+
+
+    if(k.keyCode == 38) console.log("góra");
+    if(k.keyCode == 37) console.log("lewo");
+    if(k.keyCode == 39) console.log("prawo");
+
+
+ })
+
 function display(){
-    for(let i = 0; i < (16 - (counter - 1)); i++){
-    eval(`space[tile${una[i]}.cell].innerHTML = tile${una[i]}.value`);console.log('sds');}
-}
+    for(let i in Free)
+    if(Free[i] != true) eval(`tiles[tile${i}.cell].innerHTML = tile${i}.val;`);}
+
+
+ function KeyDown(){
+
+
+ }
 //GAME FLOW
 
-
+//DONE
 //generuje tile
 //wyswietla go na ekranie
+
+
 //ruch gracza i modyfikacja obkiektów
 //sprawdzanie czy ruch jest dozwolony i czy jakis ruch jest mozliwy
-
-
-
-
-
-
-
-
-
-
-
-////////////////////////////
-
-
-//let tile1 = new tile(GRP(), GFN(), GRV());
-
-
-// function colorBoard(){}
-// tile.forEach((item, index) =>{
-//     switch(tile[index].innerHTML)
-//     {   
-//         case '2': 
-//             tile[index].style.backgroundColor = "orange";
-//         break;
-//         case '4': 
-//             tile[index].style.backgroundColor = "orange";
-//         break;
-//         case '8': 
-//             tile[index].style.backgroundColor = "orange";
-//         break;
-//         case '16': 
-//             tile[index].style.backgroundColor = "orange";
-//         break;
-//         case '32': 
-//             tile[index].style.backgroundColor = "orange";
-//         break;
-//         case '64': 
-//             tile[index].style.backgroundColor = "orange";
-//         break;
-//         case '128': 
-//             tile[index].style.backgroundColor = "orange";
-//         break;
-//         case '256': 
-//             tile[index].style.backgroundColor = "orange";
-//         break;
-//         case '512': 
-//             tile[index].style.backgroundColor = "orange";
-//         break;
-//         case '1024': 
-//             tile[index].style.backgroundColor = "orange";
-//         break;
-//         case '2048': 
-//             tile[index].style.backgroundColor = "orange";
-//         break;
-//         case '4096': 
-//             tile[index].style.backgroundColor = "orange";
-//         break;
-//         case '8192': 
-//             tile[index].style.backgroundColor = "orange";
-//         break;
-//     }
-
-//});
-
+//kolorki qrwa
